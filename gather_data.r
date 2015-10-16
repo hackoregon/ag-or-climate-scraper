fea7d38659d55709ee0a3c222d126526c0421192
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2015-10-16 07:46:44 -0700

    county page acquisition working

--- gather_data.r
+++ gather_data.r
@@ -1,1 +1,14 @@
 #' R notebook to gather data from the Oregon Climate Service website (<http://ocs.oregonstate.edu/>)
+for (p in c("rvest")) {
+  if (!require(p, character.only = TRUE)) {
+    install.packages(p)
+  }
+  require(p, character.only = TRUE)
+}
+
+session <- html_session(
+  "http://www.ocs.oregonstate.edu/county_climate/county_climate.html")
+for (i in 4:39) {
+  page <- session %>% follow_link(i)
+  print(page)
+}
